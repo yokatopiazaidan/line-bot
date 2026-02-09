f85d246761d293798d8c96d326e21058c232e673
yokatopiazaidan <yokatopiazaidan@gmail.com>
2026-02-09 12:58:03 +0900

    Update webhook.js

--- api/webhook.js
+++ api/webhook.js
@@ -1,12 +1,19 @@
-import express from "express";
-import serverless from "serverless-http";
+const express = require("express");
+const serverless = require("serverless-http");
 
 const app = express();
 app.use(express.json());
 
+// LINE の Verify が GET を送るので 200 を返す
+app.get("/webhook", (req, res) => {
+  res.status(200).send("OK");
+});
+
+// LINE の本番 Webhook は POST
 app.post("/webhook", (req, res) => {
   console.log("LINEから通知:", req.body);
   res.status(200).send("OK");
 });
 
-export const handler = serverless(app);
+module.exports = app;
+module.exports.handler = serverless(app);
